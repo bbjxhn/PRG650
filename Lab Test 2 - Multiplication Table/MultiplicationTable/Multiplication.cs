2cbf00c2e82eed3ae16923d58d35d6d52495d374
bbjxhn <jm.trinidad199@gmail.com>
2024-07-31 10:39:33 -0400

    added lab tets 2

---
+++ Lab Test 2 - Multiplication Table/MultiplicationTable/Multiplication.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MultiplicationTable
+{
+    internal class Multiplication
+    {
+        private int currentNum;
+        private Thread thread;
+        private CancellationTokenSource cancellationTokenSource;
+        private ManualResetEventSlim pauseEvent;
+
+        public Multiplication()
+        {
+            currentNum = 1;
+            cancellationTokenSource = new CancellationTokenSource();
+            pauseEvent = new ManualResetEventSlim(true);
+        }
+
+        public void GenerateTables()
+        {
+            while (!cancellationTokenSource.IsCancellationRequested)
+            {
+                pauseEvent.Wait();
+
+                Console.Write($"\nMultiplication Table of {currentNum} is:");
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.Write($"  {currentNum * i}");
+                }
+
+                currentNum++;
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void Start()
+        {
+            if (thread == null || !thread.IsAlive)
+            {
+                thread = new Thread(GenerateTables);
+                thread.Start();
+            } else
+            {
+                pauseEvent.Set();
+            }
+        }
+
+        public void Pause()
+        {
+            pauseEvent.Reset();
+        }
+
+        public void Restart()
+        {
+            currentNum = 1;
+            pauseEvent.Set(); 
+        }
+    
+
+        public void Stop()
+        {
+            cancellationTokenSource.Cancel();
+            pauseEvent.Set();
+
+            if (thread != null && thread.IsAlive)
+            {
+                thread.Join();
+            }
+        }
+    }
+}
---
+++ Lab Test 2 - Multiplication Table/MultiplicationTable/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Multiplication multiTable = new Multiplication();
+            bool running = true;
+            bool hasStarted = false;
+
+            Console.WriteLine("Press s to start or restart Printing Multiplication Tables");
+            Console.WriteLine("Press x to pause Printing Multiplication Tables");
+            Console.WriteLine("Press r to restart Printing Multiplication Tables from beginning");
+            Console.WriteLine("Press q to quit the application");
+
+            while (running)
+            {
+                char choice = Console.ReadKey(true).KeyChar;
+
+                switch (choice)
+                {
+                    case 's':
+                        if (!hasStarted)
+                        {
+                            Console.WriteLine("\nPrinting Tables Now: ");
+                            multiTable.Start();
+                            hasStarted = true;
+                        } 
+                        else
+                        {
+                            Console.WriteLine("\nPrinting has been restarted");
+                            multiTable.Start();
+                        }
+                        break;
+                    case 'x':
+                        Console.WriteLine("\nPrinting has been Paused: ");
+                        multiTable.Pause();
+                        break;
+                    case 'r':
+                        Console.WriteLine("\nPrinting tables from the beginning: ");
+                        multiTable.Restart();
+                        break;
+                    case 'q':
+                        multiTable.Stop();
+                        Console.WriteLine("\nQuitting Application");
+                        Console.WriteLine("Thread is Interrupted");
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+           
+        }
+    }
+}
